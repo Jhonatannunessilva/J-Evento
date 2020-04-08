092f1783796ea37b53e429f3ed16480cec050aab
Jhonatan Nunes da Silva <jhonatannunessilva@hotmail.com>
2020-04-08 14:49:49 -0300

    Created User Domain

---
+++ src/main/java/domain/User.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.util.UUID;
+
+/**
+ *
+ * @author jhonatan
+ */
+public class User {
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+
+    public User(String name, String email, String password) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
