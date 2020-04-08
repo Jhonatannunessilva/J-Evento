63abc24a1afc571d5817dc722713bc27a2396443
Jhonatan Nunes da Silva <jhonatannunessilva@hotmail.com>
2020-04-08 14:50:28 -0300

    Created Customer Domain

---
+++ src/main/java/domain/Customer.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ *
+ * @author jhonatan
+ */
+public class Customer {
+    private String id;
+    private String name;
+    private String cpf;
+    private List<String> phone_numbers;
+
+    public Customer(String name, String cpf, List<String> phone_numbers) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.cpf = cpf;
+        this.phone_numbers = phone_numbers;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public List<String> getPhone_numbers() {
+        return phone_numbers;
+    }
+
+    public void setPhone_numbers(List<String> phone_numbers) {
+        this.phone_numbers = phone_numbers;
+    }
+    
+}
