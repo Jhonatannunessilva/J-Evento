a708566480beb4acc08eb1cf2d157e4304299dc5
Jhonatan Nunes da Silva <jhonatannunessilva@hotmail.com>
2020-04-08 14:50:13 -0300

    Created Address Domain

---
+++ src/main/java/domain/Address.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+/**
+ *
+ * @author jhonatan
+ */
+public class Address {
+    private String street;
+    private String neighborhood;
+    private String number;
+    private String city;
+
+    public Address(String street, String neighborhood, String number, String city) {
+        this.street = street;
+        this.neighborhood = neighborhood;
+        this.number = number;
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(String neighborhood) {
+        this.neighborhood = neighborhood;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+    
+}
