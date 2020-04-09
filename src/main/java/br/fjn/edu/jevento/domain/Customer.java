0481051a17684125b70b09bfc15f26f714b6ac5d
Jhonatan Nunes da Silva <jhonatannunessilva@hotmail.com>
2020-04-08 21:44:31 -0300

    Change import package in the domains

--- src/main/java/br/fjn/edu/jevento/domain/Customer.java
+++ src/main/java/br/fjn/edu/jevento/domain/Customer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package domain;
+package br.fjn.edu.jevento.domain;
 
 import java.util.List;
 import java.util.UUID;
