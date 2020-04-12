fc790da8f8c9e96a29639a5df85091ea48a4fb15
Jhonatan Nunes da Silva <jhonatannunessilva@hotmail.com>
2020-04-12 10:58:01 -0300

    Created feature to update a Customer

--- src/main/java/br/fjn/edu/jevento/servlets/CustomerServlet.java
+++ src/main/java/br/fjn/edu/jevento/servlets/CustomerServlet.java
@@ -25,6 +25,8 @@ public class CustomerServlet extends HttpServlet {
     
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String operation = req.getParameter("operation");
+        
         String name = req.getParameter("name");
         String cpf = req.getParameter("cpf");
         String phone_number = req.getParameter("phone_number");
@@ -36,8 +38,38 @@ public class CustomerServlet extends HttpServlet {
         if (customers == null) {
             customers = new LinkedList<Customer>();
         }
-        customers.add(new Customer(name, cpf, phone_number, email));
+        
+        if (operation == "PUT"){
+            String id = req.getParameter("id");
+            
+            for (Customer customer : customers) {
+                if(customer.getId().equalsIgnoreCase(id)){
+                    customer.setName(name);
+                    customer.setCpf(cpf);
+                    customer.setPhone_number(phone_number);
+                    customer.setEmail(email);
+                    break;
+                }
+            }
+        }else {
+            customers.add(new Customer(name, cpf, phone_number, email));
+        }
         context.setAttribute("customers", customers);
         resp.sendRedirect("home.jsp");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Customer> customers = (List<Customer>) getServletContext().getAttribute("customers");
+        String customerId = req.getParameter("customerId");
+        
+        for (Customer customer : customers) {
+            if(customer.getId().equalsIgnoreCase(customerId)){
+                req.setAttribute("customerToUpdate", customer);
+                break;
+            }
+        }
+        
+        req.getRequestDispatcher("customer/update.jsp").forward(req, resp);
     }
 }
